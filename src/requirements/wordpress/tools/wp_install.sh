476a8ae14c2c81e8f6476e76fd78e2be99390901
Jeferson <jeferson23sopan@gmail.com>
2024-11-14 16:29:48 +0100

    fixing wp_install.sh

--- src/requirements/wordpress/tools/wp_install.sh
+++ src/requirements/wordpress/tools/wp_install.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-
-# Navigate to WordPress directory
 cd /var/www/html/wordpress
 
-wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
-        --dbhost=mariadb:3306 --allow-root
+wp core config --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
+        --allow-root
 
-wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER \
-        --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --title=$WP_TITLE --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --url=$WP_URL --allow-root
 
-wp user create $WP_USER $WP_USER_EMAIL --role=AUTHOR --user_pass=$WP_USER_PWD --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
+cd-
 
-# Start PHP-FPM
 php-fpm7.3 -F
