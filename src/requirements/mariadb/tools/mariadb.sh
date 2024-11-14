fdba3bf5fe0c434721522acbd4582a5d595f138a
Jeferson <jeferson23sopan@gmail.com>
2024-11-14 15:05:55 +0100

    changing dockercompose file

--- src/requirements/mariadb/tools/mariadb.sh
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -8,7 +8,3 @@ mysql -u root -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PA
 mysql -u root -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
-
-service mysql stop
-
-exec mysqld_safe
